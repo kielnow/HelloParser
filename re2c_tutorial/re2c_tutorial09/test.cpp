83465202bae5b131e056d9370ec01e3113855466
kielnow <etsu1here@gmail.com>
2015-08-02 18:11:58 +0900

    re2cのチュートリアル(strip)を追加

---
+++ re2c_tutorial/re2c_tutorial09/test.cpp
@@ -0,0 +1,12 @@
+/*
+	hello, world!
+	2015/08/02
+*/
+#include <iostream>
+
+int main()
+{
+	// hello, world!
+	std::cout << "hello, world!" << std::endl;
+	return 0;
+}
---
+++ re2c_tutorial/re2c_tutorial10/test.cpp
@@ -0,0 +1,15 @@
+/*
+	hello, world!
+	2015/08/02
+*/
+/*
+	test
+*/
+#include <iostream>
+
+int main()
+{
+	// hello, world!
+	std::cout << "hello, world!" << std::endl;
+	return 0;
+}
